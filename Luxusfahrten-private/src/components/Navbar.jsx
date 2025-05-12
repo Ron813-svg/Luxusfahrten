cdd7c698215d38cb2c5cfc13f928195685defbbb
Rony <ronyjavierramirezalvarado@gmail.com>
2025-05-12 08:16:12 -0600

    Termine unas cosas

--- Luxusfahrten-private/src/components/Navbar.jsx
+++ Luxusfahrten-private/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navbar = () => {
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
@@ -64,11 +65,11 @@ const Navbar = () => {
                         
                         {/* Botón de login siempre visible */}
                         <Link to="/login" className="ms-lg-3">
-                            <button className="btn btn-outline-light rounded-circle p-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
-                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                                </svg>
-                            </button>
+                            
+                        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+      <span className="material-symbols-outlined" style={{ color: 'white' }}>logout</span>
+
+
                         </Link>
                     </div>
                 </div>
